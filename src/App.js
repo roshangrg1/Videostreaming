c7b49ae06242b922d6cc206cd97dc756441104c0
Roshan Guragain <twistfreaky@gmail.com>
2023-05-17 10:46:56 +0545

    react components added with route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      app
+import Navbar from './components/Navbar/Navbar'
+import Feed from './components/Feed/Feed'
+import VideoDetails from './components/VideoDetails/VideoDetails'
+import ChannelDetails from './components/ChannelDetails/ChannelDetails'
+import SearchFeed from './components/SearchFeed/SearchFeed'
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Feed/>}/>
+          <Route path='/video/:id' element={<VideoDetails/>}/>
+          <Route path='/channel/:id' element={<ChannelDetails/>}/>
+          <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+        </Routes>
     </div>
-  );
-}
+  </BrowserRouter>
+)
 
-export default App;
+
+
+export default App
