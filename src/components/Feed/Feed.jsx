666802e74fd72668b8a1feb21ac4b0ff04e78e40
Roshan Guragain <twistfreaky@gmail.com>
2023-05-18 14:02:52 +0545

    Siderbar added to the feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../Sidebar/Sidebar'
+import './Feed.css'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div className='feed-container'>
+      <div className="left">
+        <Sidebar/>
+        <p className='copyright'>copyright @roshan Guragain</p>
+      </div>
+      <div className="right">
+        video container
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Sidebar.css'
+import { AiFillCode, AiFillDollarCircle, AiFillHome } from 'react-icons/ai'
+import { TiNews } from 'react-icons/ti'
+import { SiYoutubemusic, SiApplepodcasts, SiPcgamingwiki } from 'react-icons/si'
+import { MdCastForEducation, MdMovie, MdLiveTv, MdSportsHandball, MdFace, MdTheaterComedy, MdOutlineSportsGymnastics } from 'react-icons/md'
+import { GiClothes } from 'react-icons/gi'
+import { BsCashCoin } from 'react-icons/bs'
+
+
+const categories = [
+  { name: 'New', icon: <AiFillHome />, },
+  { name: 'Finance', icon: <AiFillDollarCircle />, },
+  { name: 'Coding', icon: <AiFillCode />, },
+  { name: 'News', icon: <TiNews />, },
+  { name: 'Music', icon: <SiYoutubemusic /> },
+  { name: 'Education', icon: <MdCastForEducation />, },
+  { name: 'Podcast', icon: <SiApplepodcasts />, },
+  { name: 'Movie', icon: <MdMovie />, },
+  { name: 'Gaming', icon: <SiPcgamingwiki />, },
+  { name: 'Live', icon: <MdLiveTv />, },
+  { name: 'Sport', icon: <MdSportsHandball />, },
+  { name: 'Fashion', icon: <GiClothes />, },
+  { name: 'Beauty', icon: <MdFace />, },
+  { name: 'Comedy', icon: <MdTheaterComedy />, },
+  { name: 'Gym', icon: <MdOutlineSportsGymnastics />, },
+  { name: 'Crypto', icon: <BsCashCoin />, },
+];
+
+const selectedCategory = 'Crypto';
+
+const Sidebar = () => (
+  <div className="sidebar-container" >
+    {categories.map((category) => (
+      <button className='sidebar-item'
+        style={{ background: `${category.name === selectedCategory && 'rgb(53, 53, 188)'}` }}
+        key={category.name}>
+        <span className='span-icon' style={{color:`${category.name=== selectedCategory ? 'white' :'rgb(53, 53, 188'}`}}>{category.icon}</span>
+        <span style={{opacity:`${category.name=== selectedCategory ? '1' :'0.5'}`}}> {category.name}</span>
+      </button>
+    )
+    )}
+  </div>
+)
+
+
+
+export default Sidebar
