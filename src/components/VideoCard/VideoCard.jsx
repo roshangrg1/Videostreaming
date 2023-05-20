c27ad167d96d830151fb3f4a03f56a1a785107c5
Roshan Guragain <twistfreaky@gmail.com>
2023-05-20 14:10:49 +0545

    Videos added

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { AiFillCheckCircle } from 'react-icons/ai'
 
-const VideoCard = ({video}) => {
+import './VideoCard.css'
+
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+  // console.log(videoId, snippetLink
   return (
-    <div>VideoCard</div>
+    <div className='video-card-container'>
+      <Link to={videoId ? `/video/${videoId}` : "notfound"} >
+        <img src={snippet?.thumbnails?.high?.url} alt="" style={{ height: '180px', width: '300px' }} />
+      </Link>
+      <div className="video-content" >
+        <Link to={videoId ? `/video/${videoId}` : "notfound"} style={{marginBottom:'20px'}} >
+          <h5>{snippet?.title.slice(0, 60) || 'demo failed to load'}</h5>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : "notfound"}  style={{marginBottom:'20px'}} >
+          <p>{snippet?.channelTitle || 'demo failed to load'}
+            <AiFillCheckCircle /></p>
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
+import './Videos.css'
 import VideoCard from '../VideoCard/VideoCard'
 import ChannelCard from '../ChannelCard/ChannelCard'
 
 const Videos = ({ videos }) => {
   console.log(videos)
   return (
-    <div >
+    <div className='videos-container'>
       {videos.map((item , idx)=>(
-        <div key={idx}>
+        <div key={idx} >
           {item.id.videoId && <VideoCard video={item}/>}
           {/* {item.snippet.channelId && <ChannelCard channelDetail={item}/>} */}
         </div>
