435a22a83ebea3041d4d465b169f80f57de63bbf
Roshan Guragain <twistfreaky@gmail.com>
2023-05-20 22:14:46 +0545

    added search query

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import {BsSearch} from 'react-icons/bs'
+import { BsSearch } from 'react-icons/bs'
 
 import './SearchBar.css'
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) =>{
+    e.preventDefault(); // prevents from reloading
+
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('')
+    }
+  }
   return (
-    <form className ="searchbar-container" onSubmit={()=> {}}>
-        <input className='search-input' type="text" placeholder='Search'/>
-        <button className='submit-button'><BsSearch/></button>
-        
+    <form className="searchbar-container" onSubmit={handleSubmit}>
+      <input className='search-input' type="text" placeholder='Search'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} />
+      <button className='submit-button'><BsSearch /></button>
+
     </form>
   )
 }
--- src/components/SearchFeed/SearchFeed.jsx
+++ src/components/SearchFeed/SearchFeed.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Videos from '../Videos/Videos'
+import { useParams } from 'react-router-dom'
+
+import { fetchFromApi } from '../../utils/api'
+
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams()
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => {
+        setVideos(data.items)
+      })
+  }, [searchTerm])
   return (
-    <div>SearchFeed</div>
+    <div className="search-feed">
+
+      <Videos videos={videos} />
+    </div>
   )
 }
 
