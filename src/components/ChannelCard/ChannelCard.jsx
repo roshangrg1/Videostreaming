be726e24a8ed6f1494515d336685ddb1526e6470
Roshan Guragain <twistfreaky@gmail.com>
2023-05-20 21:37:45 +0545

    channel details added

--- src/components/ChannelCard/ChannelCard.jsx
+++ src/components/ChannelCard/ChannelCard.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './ChannelCard.css'
 import { AiFillCheckCircle } from 'react-icons/ai'
-const ChannelCard = ({channelDetail}) => {
+const ChannelCard = ({channelDetail, marginTop}) => {
   return (
-    <div className="channel-card" >
-      <Link to={`/channel/${channelDetail?.id?.channelId}`} className='channel-card-item'>
+    <div className="channel-card" style={{marginTop}} >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <div className="card-content">
           <div className="img-container">
           <img src={channelDetail?.snippet?.thumbnails?.high?.url} alt="" />
           </div>
           
           <h5>{channelDetail?.snippet?.title} <AiFillCheckCircle/></h5>
+          {channelDetail?.statistics?.subscriberCount && (
+            <p style={{color:'#fff'}}>{channelDetail?.statistics?.subscriberCount } subscriber</p>
+          )}
         </div>
       </Link>
     </div>
--- src/components/ChannelDetails/ChannelDetails.jsx
+++ src/components/ChannelDetails/ChannelDetails.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchFromApi } from '../../utils/api';
+import ChannelCard from '../ChannelCard/ChannelCard';
+import Videos from '../Videos/Videos';
 
 const ChannelDetails = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+  const { id } = useParams();
+
+  console.log(channelDetail, videos)
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
+
+  }, [id])
   return (
-    <div>ChannelDetails</div>
+    <div className='channel-details-container'>
+      <div className="">
+        <div className="cover" style={{ height: '300px', background: '#F0DEDE' }}></div>
+        <ChannelCard channelDetail={channelDetail} marginTop={'-50px'} />
+      </div>
+
+      <div>
+        <div>
+          <Videos videos={videos} />
+        </div>
+
+      </div>
+    </div>
+
   )
 }
 
